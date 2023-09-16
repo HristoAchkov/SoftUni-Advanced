158363ab4ed019f218a661f4a85078ceed25c87c
Hristo Achkov <122983973+HristoAchkov@users.noreply.github.com>
2023-09-16 12:01:43 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Exercise/1.UniqueUsernames.cs
@@ -0,0 +1,14 @@
+﻿int entries = int.Parse(Console.ReadLine());
+
+HashSet<string> set = new HashSet<string>();
+
+for (int i = 0; i < entries; i++)
+{
+    string name = Console.ReadLine();
+
+    set.Add(name);
+}
+foreach (var item in set)
+{
+    Console.WriteLine(item);
+}
